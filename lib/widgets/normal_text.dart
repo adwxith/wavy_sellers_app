44edec1d6bc5d8f0da400cb5d9f0d6d7d0e7df1b
adwxith <21c05.adwaith@sjec.ac.in>
2023-04-25 15:34:39 +0530

    text style and text colour declraration

--- lib/widgets/normal_text.dart
+++ lib/widgets/normal_text.dart
@@ -1,5 +1,9 @@
 import 'package:wavy_sellers_app/const/const.dart';
 
-Widget normalText({text, Color}) {
-  return "$text".text.make();
+Widget normalText({text, Color = Colors.white}) {
+  return "$text".text.color(Color).make();
+}
+
+Widget boldText({text, Color = Colors.white, Size = 14}) {
+  return "$text".text.semiBold.color(Color).make();
 }
