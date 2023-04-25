757dbb1b0f8528c209c6daba3f5a2b0b18d4a3ff
adwxith <21c05.adwaith@sjec.ac.in>
2023-04-25 12:29:53 +0530

    firebase initialisation

--- lib/const/strings.dart
+++ lib/const/strings.dart
@@ -1,8 +1,8 @@
-const appname = "eMart Seller App",
+const appname = "Wavy Seller App",
     login = "Login",
     welcome = "Hi, Welcome to",
     email = "Email",
     password = "Password",
     forgotPassword = "Forgot Password",
     anyProblem = "In case of any difficulty, contact administration",
-    credit = "@BaabaDevs";
+    credit = "@wavy";
